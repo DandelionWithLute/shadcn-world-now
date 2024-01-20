62e3388944b4e60f100ed56bd7f3f81baaba9c65
OrangeBirdDrinksWater <114991717+OrangeBirdDrinksWater@users.noreply.github.com>
2024-01-20 22:29:38 +0800

    new
    new

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,61 +5,81 @@ import CredentialsProvider from "next-auth/providers/credentials";
 // import { PrismaAdapter } from "@auth/prisma-adapter";
 import bcrypt from "bcryptjs";
 import prisma from "@/lib/connect";
-import GitHub from "next-auth/providers/github"
+import GitHub from "next-auth/providers/github";
+import { authConfig } from "./auth.config";
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-    // adapter: PrismaAdapter(prisma),
-    providers: [
-      CredentialsProvider({
-        id: "credentials",
-        name: "Credentials",
-        async authorize(credentials) {
-          //Check if the user exists.
-          try {
-            const user = await prisma.user.findUnique({
-              where: { email: credentials.email },
-            });
-  
-            if (user) {
-              const isPasswordCorrect = await bcrypt.compare(
-                credentials.password,
-                user.hashedPassword
-              );
-  
-              if (isPasswordCorrect) {
-                console.log("Password Correct!");
-                return user;
-              } else {
-                throw new Error("Wrong Credentials!");
-              }
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        //Check if the user exists.
+        try {
+          const user = await prisma.user.findUnique({
+            where: { email: credentials.email },
+          });
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.hashedPassword
+            );
+
+            if (isPasswordCorrect) {
+              console.log("Password Correct!");
+              return user;
             } else {
-              throw new Error("User not found!");
+              throw new Error("Wrong Credentials!");
             }
-          } catch (err) {
-            throw new Error();
+          } else {
+            throw new Error("User not found!");
           }
-        },
-      }),
-      GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-      }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-    ],
-    pages: {
-      error: "/dashboard/login",
+        } catch (err) {
+          throw new Error();
+        }
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  pages: {
+    error: "/dashboard/login",
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider === "github") {
+        try {
+          const user = await prisma.user.findUnique({ email: profile.email });
+
+          if (!user) {
+            prisma.user.create({
+              data: {
+                username: profile.login,
+                email: profile.email,
+                image: profile.avatar_url,
+              },
+            });
+          }
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }
+      return true;
     },
-  })
-
-
-
-
-
+    ...authConfig.callbacks,
+  },
+});
 
 // export { handler as GET, handler as POST };
